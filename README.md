e2a04a8b33eb5b1beca70bf86b60ceafdf10ed41
Valeriy Falshtinskiy <Falstinskyvalery@gmail.com>
2026-01-28 15:34:31 +0300

    create: brain-even.js, demo.cast && modified: Makefile, README.md, package.json, package-lock.json

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Gordarick/fullstack-python-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Gordarick/fullstack-python-project-44/actions)
+Запись asciinema:
+https://asciinema.org/connect/952097e3-e4c5-400c-abcc-a5af196d2f50
+
 Команды:
 install - выполняет установку зависимостей.
 brain-games - запуск игры.
 publish - do something.
 lint - запуск eslint
+
---
+++ bin/brain-even.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+//const readlineSync = require('readline-sync');
+////const userName = readlineSync.question('May I have your name? ');
+////console.log('Hello, ' + userName + '!')
+import readlineSync from 'readline-sync'
+
+const startGame = () => {
+	console.log('Welcome to the Brain Even!')
+	const playerName = readlineSync.question('May I have your name? ')
+	console.log('Hello, ' + playerName + '!')
+	console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+	const numOfGames = 3
+
+	for (let i = 0; i < numOfGames; i++) {
+		const num = Math.floor(Math.random() * 100)
+		const answer = readlineSync.question('Question: ' + num + '\nYour answer (yes/no): ')
+		const isEven = num % 2 === 0
+		const correctAnswer = isEven ? 'yes' : 'no'
+	if (answer.toLowerCase() !== correctAnswer) {
+		console.log(answer + ' is wrong answer ;(. Correct answer was ' + correctAnswer +'.')
+		console.log('Let\'s try again, ' + playerName + '!')
+		return
+	}
+		console.log('Correct!')
+	}
+	console.log('Congratulations, ' + playerName + '!')
+}
+
+		
+startGame();
